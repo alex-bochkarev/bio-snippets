563aef1dc41ff5552d875765f744e459ad39b078
Alexey Bochkraev <a@bochkarev.io>
2020-05-04 23:21:57 +0200

    added no. of processes to GNU Parallel example

--- run_for_every_file_parallel.sh
+++ run_for_every_file_parallel.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-ls ./data/* | parallel "./parallel_script.sh {}"
+# The following will run the ./parallel_script.sh taking
+# each file in "./data/" as the first (and only) argument.
+# Will try to run 4 processes in parallel
+
+ls ./data/* | parallel -j4 "./parallel_script.sh {}"
+
